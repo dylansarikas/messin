5f6eb486ac5936027f86aafc5dc4181fff242951
Dylan Sarikas <97060448+dylansarikas@users.noreply.github.com>
2024-04-03 18:43:09 +0000

    adds rando

--- rando.rb
+++ rando.rb
@@ -1,6 +1,21 @@
 # Given an array of intergers and a number, return that number of integers from the array randomly
 
-def rando
-
+def rando(original, amount)
+    numbers = original
+    length = numbers.length
+    output = []
+    amount.times do
+        #p numbers
+        random = rand(length)
+        #p random
+        output.push(numbers[random])
+        numbers.delete_at(random)
+        length -= 1
+        #p numbers
+    end
+    p output
 
 end
+
+numbers = [12, 43, 23, 56]
+rando(numbers, 2)
